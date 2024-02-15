c0c3b6a2f9c061715bf5f2e07fdbe0669c00e5ff
Angelica <139031564+Angiedoescoding@users.noreply.github.com>
2024-02-15 15:57:53 -0800

    express require and port added

---
+++ server.js
@@ -0,0 +1,21 @@
+// I put required express library
+
+const express = require("express");
+
+// // I run express and stored return value in app variable
+const app = express()
+
+// Save port number as variable for dynamic purposes
+let PORT = 3000
+
+
+
+// Middleware
+
+
+
+
+//Exploring Response Options
+
+
+// Open Exploration
