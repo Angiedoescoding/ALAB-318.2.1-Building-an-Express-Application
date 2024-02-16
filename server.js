daad9690a2037dd8e6a816ab3d3a55e4e98476e4
Angelica <139031564+Angiedoescoding@users.noreply.github.com>
2024-02-15 16:56:31 -0800

    app.listen port

--- server.js
+++ server.js
@@ -2,11 +2,11 @@
 
 const express = require("express");
 
-// // I run express and stored return value in app variable
-const app = express()
+// I run express and stored return value in app variable
+const app = express();                      // we create an application by running (calling) express() and caching the return as a variable, typically called app.
 
 // Save port number as variable for dynamic purposes
-let PORT = 3000
+let PORT = 3000;
 
 
 
@@ -19,3 +19,9 @@ let PORT = 3000
 
 
 // Open Exploration
+
+// We must listen to the server on specified PORT
+
+app.listen(PORT, () =>{
+    console.log(`Server listening (alab task): ${PORT}`)
+})
